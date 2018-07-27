85e89ecea8a42563d7c0ec76f128282b0cb60846
Diego <diego.lgrip@gmail.com>
2018-07-27 18:39:24 +0200

    cli is now a module and main.rs can use it

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 extern crate unicore_p2p as p2p;
+extern crate unicore_cli as cli;
 
 use p2p::{Client, Server};
 
@@ -6,6 +7,7 @@ fn main() {
     println!("Hello, world!");
     let _client = Client::new(80);
     let _server = Server::new(80);
+    cli::cli::cli();
     _server.start();
     _client.connect();
 }
