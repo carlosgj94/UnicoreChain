322e74262522e57a8165dddcd1d81c90edeae7e9
carlosgj94 <carlosgj94@gmail.com>
2018-07-28 21:14:42 +0200

    Adding the constructor for the address

--- keys/src/address.rs
+++ keys/src/address.rs
@@ -1,5 +1,29 @@
+use base58::FromBase58;
+use secp256k1::Error;
+use std::str::FromStr;
 use AddressHash;
 
 pub struct Address {
     pub hash: AddressHash,
+}
+
+impl FromStr for Address {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Error> {
+        let hex = try!(s.from_base58().map_err(|_| Error::InvalidSecretKey));
+
+        if hex.len() != 25 {
+            return Err(Error::InvalidSecretKey);
+        }
+
+        let hash = AddressHash::from_str(s);
+
+        match hash {
+            Ok(v) => {
+                return Ok(Address { hash: v });
+            }
+            Err(_) => return Err(Error::InvalidPublicKey),
+        }
+    }
 }
