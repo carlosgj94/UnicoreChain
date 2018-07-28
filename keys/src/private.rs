20cf217580b82e83a3ca72ae0e0736f7c4102266
carlosgj94 <carlosgj94@gmail.com>
2018-07-28 13:55:36 +0200

    Initial version of the private key

--- keys/src/private.rs
+++ keys/src/private.rs
@@ -1,9 +1,38 @@
-use Secret;
+use base58::FromBase58;
+use secp256k1::Error;
+use std::str::FromStr;
+use {Message, Secret, Signature};
 
 #[derive(PartialEq)]
 pub struct Private {
-    /// The network where the key is being used
-    pub network: u32,
     /// ECDSA key.
     pub secret: Secret,
+}
+
+impl Private {
+    /// Function that signs a message and returns the signature
+    pub fn sign(&self, _message: &Message) -> Signature {
+        unimplemented!();
+    }
+}
+
+impl FromStr for Private {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Error> {
+        let hex = try!(s.from_base58().map_err(|_| Error::InvalidSecretKey));
+
+        if hex.len() != 33 {
+            return Err(Error::InvalidSecretKey);
+        }
+
+        let secret = Secret::from_str(s);
+
+        match secret {
+            Ok(v) => {
+                return Ok(Private { secret: v });
+            }
+            Err(_) => return Err(Error::InvalidSecretKey),
+        }
+    }
 }
